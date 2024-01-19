89fed9e1c4ccd93dd04df4a707d2829f5ade306b
r2hu1 <newasram86@gmail.com>
2024-01-19 16:39:14 +0530

    [bug fixes]

--- app/_components/Trans.jsx
+++ app/_components/Trans.jsx
@@ -1,10 +1,11 @@
-import { getHist } from "@/lib/create"
+import { getFromHist, getHist } from "@/lib/create"
 import { currentUser } from "@clerk/nextjs";
 
 export default async function Trans() {
     const user = await currentUser();
     const email = user.emailAddresses[0].emailAddress.split("@")[0] + "@zpi";
     const trans = await getHist();
+    const fromTrans = await getFromHist();
 
     return (
         <div className="grid gap-3 mt-20">
@@ -20,7 +21,15 @@ export default async function Trans() {
                         ) : (
                             <h1 className="text-md">{item.amount}<span className="text-xs text-primary">Z</span> to {item.to}</h1>
                         )}
-                        {/* <span>{item}</span> */}
+                    </div>
+                ))}
+                {fromTrans.map((item, index) => (
+                    <div key={index} className="w-full flex items-center justify-between h-fit md:max-w-sm py-4 px-5 bg-background rounded-md border">
+                        {item.from != email ? (
+                            <h1 className="text-md">{item.amount}<span className="text-xs text-primary">Z</span> from {item.from}</h1>
+                        ) : (
+                            <h1 className="text-md">{item.amount}<span className="text-xs text-primary">Z</span> to {item.to}</h1>
+                        )}
                     </div>
                 ))}
             </div>
