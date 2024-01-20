3010900a41530691129671e86286c32c4e21376d
r2hu1 <newasram86@gmail.com>
2024-01-20 23:27:32 +0530

    [added qrcode]

--- app/(user)/_components/RequestBtn.jsx
+++ app/(user)/_components/RequestBtn.jsx
@@ -1,12 +1,56 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { ArrowDown, Plus } from "lucide-react";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import Zpi from "./Zpi";
 import { toast } from "sonner";
+import domtoimage from 'dom-to-image';
+import { useRef, useEffect } from 'react';
 
 export default function RequestBtn({ zpiid }) {
+    const qrcodeRef = useRef(null);
+    const zpiQr = `https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=https://zenpay.vercel.app/dashboard/pay?zpiid=${zpiid}`;
+    const handleDownload = () => {
+        toast.loading("downloading");
+        domtoimage.toJpeg(qrcodeRef.current, { quality: 0.95 })
+            .then(function (dataUrl) {
+                var link = document.createElement('a');
+                link.download = 'qrcode.jpeg';
+                link.href = dataUrl;
+                link.click();
+                toast.success("downloaded");
+            })
+    };
+
     return (
         <>
-            <Button onClick={() => { navigator.clipboard.writeText(zpiid); toast.success("ZPI copied!") }} className="flex items-center justify-center">Receive<ArrowDown className="h-4 w-4" /></Button>
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Button className="flex items-center justify-center">Receive<ArrowDown className="h-4 w-4" /></Button>
+                </DialogTrigger>
+                <DialogContent ref={qrcodeRef} className="rounded-md">
+                    <DialogHeader className="mt-7">
+                        <DialogTitle className="text-xl -mb-1">Scan And Pay</DialogTitle>
+                        <DialogDescription className="py-5 grid gap-4">
+                            <p className="text-center text-xs -mt-6">Scan this QR code using zenpay app <br /> and send me zen coin</p>
+                            <Zpi zpi={zpiid} showshare={false} />
+                            <img src={zpiQr} alt="qrcode" className="mx-auto rounded-md h-[250px] w-[250px] bg-secondary" />
+                            <div className="flex gap-2 mt-4">
+                                <Button className="w-full" onClick={() => { navigator.share({ title: "Scan and Pay", url: zpiQr, text: `Scan this QR code and send me zen coin!` }) }}>Share</Button>
+                                <Button className="w-full" onClick={handleDownload} variant="secondary">Save</Button>
+                            </div>
+                        </DialogDescription>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
+
             <Button variant="secondary" onClick={() => { navigator.share({ text: `Send me some zen coin on my zpi id: ${zpiid}`, title: "ZenPay", url: "https://zenpay.vercel.app" }); }} className="flex items-center justify-center gap-[2px]" size="icon"><Plus className="h-4 w-4" /></Button>
         </>
     )
--- app/(user)/_components/Zpi.jsx
+++ app/(user)/_components/Zpi.jsx
@@ -1,14 +1,16 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
-import { Share } from "lucide-react";
+import { Share2 } from "lucide-react";
 import { toast } from "sonner";
 
-export default function Zpi({ zpi }) {
+export default function Zpi({ zpi, showshare = true }) {
     return (
         <div className="flex gap-2 items-center justify-center">
             <span onClick={(e) => { navigator.clipboard.writeText(e.target.textContent); toast.success("ZPI copied!") }} className="bg-secondary border select-none hover:text-white px-4 py-2 text-sm rounded-full hover:bg-primary cursor-pointer transition">{zpi}</span>
-            <Button size="icon" className="rounded-full" onClick={() => { navigator.share({ title: "ZenPay", url: "https://zenpay.vercel.app" }); }}><Share className="h-4 w-4" /></Button>
+            {showshare && (
+                <Button size="icon" className="rounded-full" onClick={() => { navigator.share({ title: "ZenPay", url: "https://zenpay.vercel.app" }); }}><Share2 className="h-4 w-4" /></Button>
+            )}
         </div>
     )
 }
