38b77207655c4935b2bd74aa83a684039b064b1a
r2hu1 <newasram86@gmail.com>
2024-01-21 21:36:54 +0530

    [some tweaks]

--- components/ModeToggle.jsx
+++ components/ModeToggle.jsx
@@ -12,12 +12,12 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
-export function ModeToggle() {
+export function ModeToggle({className}) {
     const { setTheme } = useTheme()
 
     return (
         <DropdownMenu>
-            <DropdownMenuTrigger asChild>
+            <DropdownMenuTrigger asChild className={className}>
                 <Button variant="outline" size="icon">
                     <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                     <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
